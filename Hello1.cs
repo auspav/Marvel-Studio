5b1bd2ec80ca20d1fd8ee308fd8ea67905c9bfe8
marty80 <marty80@gmail.com>
2013-12-30 22:08:47 +0800

    Update Hello1.cs

--- Hello1.cs
+++ Hello1.cs
@@ -3,6 +3,10 @@ public class Hello1
 {
    public static void Main()
    {
-      System.Console.WriteLine("Hello, Marvel!");
+      System.Console.WriteLine("Knock, knock!");
+      System.Console.WriteLine("Who's there?");
+      System.Console.WriteLine("Hello.");
+      System.Console.WriteLine("Hello who?");
+      System.Console.WriteLine("Hello Marvel!");
    }
 }
